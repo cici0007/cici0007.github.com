764e7af09fe4a923775c3f05c765d9e85b6f3de0
cici wong <2693995851@qq.com>
2022-06-10 17:44:56 +0800

    Update SetFileTime.cpp

--- code/SetFileTime.cpp
+++ code/SetFileTime.cpp
@@ -2,27 +2,32 @@
 
 int main()
 {
-    SYSTEMTIME systime;
-    const char *dt="20220610164713587";
-    sscanf(dt,"%04hd%02hd%02hd%02hd%02hd"
-    
+    SYSTEMTIME sdt;
+    memset(&sdt,0,sizeof(sdt));
+    const char *cdt="20220610164713587";
+    sscanf(cdt,"%04hd%02hd%02hd%02hd%02hd%02d%03hd",
+           &sdt.wYear,&sdt.wMonth,&sdt.wDay,
+           &sdt.wHour,&sdt.wMinute,&sdt.wSecond,
+           &sdt.wMilliseconds);
     FILETIME ft;
     SystemTimeToFileTime(&systime, &ft);
-    LocalFileTimeToFileTime(&ft,&ft);
+    LocalFileTimeToFileTime(&ft,&ft);//utc+8 ---> utc
     
+    const char *path = "0001.jpg";
     HANDLE hFile = CreateFileA(
-        argv[1],
-        GENERIC_READ | GENERIC_WRITE,
-        FILE_SHARE_READ| FILE_SHARE_WRITE,
-        NULL,
-        OPEN_EXISTING,
+        path,
+        //GENERIC_READ|GENERIC_WRITE,        
+        //FILE_GENERIC_READ|FILE_GENERIC_WRITE,
+        FILE_WRTIE_ATTRIBUTE,//dedired write file attribute only
+        NULL,NULL,OPEN_EXISTING,//open existing file only
         FILE_ATTRIBUTE_NORMAL,
         NULL);
-    if (hFile== INVALID_HANDLE_VALUE)
+    if (INVALID_HANDLE_VALUE==)
     {
-        cout<<"打开文件失败\n";
+        DWORD err = GetLastError();
+        //balabala
     }
-    SetFileTime(hFile, NULL, &ft1, &ft1);
+    SetFileTime(hFile,&ft,&ft,&ft1);
     CloseHandle(hFile);
     
     return 0;
